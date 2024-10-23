3f4f3b0ff89506cf14988a41384e22e9768de2cd
USMCrules02 <cally.piland@eagles.oc.edu>
2024-10-23 10:28:47 -0500

    Fixed - compenent and parent comunication

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,45 +1,14 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { toRefs, ref, reactive } from 'vue';
 import { mdiMenuRight } from '@mdi/js';
-
 const required = (label) => (value) => !!value || `The ${label} field is required.`;
-
-const Person = reactive({
-    id: 0,
-    fName: '',
-    lName: ''
-});
-
-const ContactInfo = reactive({
-    id: 0,
-    Email: '',
-    PhoneNumber: '',
-    Address: ''
-});
-
-const personalInfoForm = ref(null);
-const emit = defineEmits(['submitFormData']);
-
-const savePersonalInfo = () => {
-    personalInfoForm.value.validate().then((isValid) => {
-        if (isValid) {
-            emit('submitFormData', { Person, ContactInfo });
-            //Add backend submit here.
-            console.log(1)
-        } else {
-            console.log('Form validation failed.');
-        }
-    });
-};
-
 </script>
-
 <template>
     <div>
-        <v-form ref="personalInfoForm" @submit.prevent="savePersonalInfo">
+        <v-form ref="personalInfoForm">
             <v-row align="center" justify="center">
                 <v-card class="mx-auto" max-width="400">
-                    <v-card-title style="text-align: center;">
+                    <v-card-title style="text-align:center">
                         <span class="text-h6">Personal Information</span>
                     </v-card-title>
                     <v-divider class="mx-4"></v-divider>
@@ -49,22 +18,18 @@ const savePersonalInfo = () => {
                                 <v-text-field class="formField" v-model="Person.fName" label="First Name"
                                     :rules="[required('First Name')]"></v-text-field>
                             </v-col>
-
                             <v-col cols="6">
                                 <v-text-field class="formField" v-model="Person.lName" label="Last Name"
                                     :rules="[required('Last Name')]"></v-text-field>
                             </v-col>
-
                             <v-col cols="12">
                                 <v-text-field class="formField" v-model="ContactInfo.Email" label="Email"
                                     :rules="[required('Email')]"></v-text-field>
                             </v-col>
-
                             <v-col cols="12">
                                 <v-text-field class="formField" v-model="ContactInfo.PhoneNumber" label="Phone Number"
                                     :rules="[required('Phone Number')]"></v-text-field>
                             </v-col>
-
                             <v-col cols="12">
                                 <v-text-field class="formField" v-model="ContactInfo.Address" label="Address"
                                     :rules="[required('Address')]"></v-text-field>
@@ -74,7 +39,7 @@ const savePersonalInfo = () => {
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <div style="float:right">
-                            <v-btn type="submit">
+                            <v-btn @click="submitForm">
                                 <v-icon icon="mdi-chevron-right" style="font-size: 30px;"></v-icon>
                             </v-btn>
                         </div>
@@ -84,3 +49,28 @@ const savePersonalInfo = () => {
         </v-form>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        Person: {
+            type: Object,
+        },
+        ContactInfo: {
+            type: Object,
+        },
+    },
+    methods:{
+        async submitForm() {
+            const personalInfoForm = ref(null)
+            const { valid } = await this.$refs.form.validate();
+            if (valid) {
+                this.emit('PersonalInfo', { Person, ContactInfo });
+            }
+            else {
+                console.log('Form validation failed.');
+            }
+        }
+    }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,10 @@
 <script setup>
-import SocialLogin from "../components/SocialLogin.vue";
-import { VIcon } from '../../node_modules/vuetify/lib/components/VIcon'
-import { ref } from 'vue';
-var location = 0;
-const formData = ref(null);
-
-const handleFormSubmission = (data) => {
-  console.log('Form data from child:', data);
-}
-
+import PersonalInfo from "../components/PersonalInfo.vue"
 </script>
-
 <template>
   <v-container>
     <br /><br />
     <SocialLogin />
     <br /><br />
-    <div v-if="location == 0">
-      <PersonalInfo @submitFormData="handleFormSubmission" />
-    </div>
   </v-container>
 </template>
-
-
-<script>
-import PersonalInfo from "../components/PersonalInfo.vue"
-
-export default {
-  components: {
-    PersonalInfo,
-  }
-};
-</script>
