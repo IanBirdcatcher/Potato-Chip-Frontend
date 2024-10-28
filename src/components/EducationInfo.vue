fc2cdffd935ac1a94b5262dc9a05d04e32c58065
USMCrules02 <cally.piland@eagles.oc.edu>
2024-10-28 15:07:36 -0500

    Added - conditional scroll

--- src/components/EducationInfo.vue
+++ src/components/EducationInfo.vue
@@ -4,8 +4,8 @@ const required = (label) => (value) => !!value || `The ${label} field is require
 <template>
     <div>
         <v-form ref="educationForm">
-            <v-row align="center" justify="center">
-                <v-card class="mx-auto" width="400" height="515" style="overflow-y: scroll">
+            <v-row align="center" justify="center" >
+                <v-card class="mx-auto" width="400" height="515" :style="[Education.length > 1 ? 'overflow-y: scroll' : '']">
                     <v-card-title style="text-align:center">
                         <span class="text-h6">Education</span>
                     </v-card-title>
