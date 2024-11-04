dadeeb0bc536a084edd5ded5c0ec71207cc3dc06
Sage Myers <sage.myers@eagles.oc.edu>
2024-11-04 17:15:53 -0600

    home page visuals

--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Login from "./views/Login.vue";
+import HomePage from "./views/HomePage.vue";
 import CreateResume from "./views/CreateResume.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { 
+      path: "/home", 
+      alias: "/homePage",
+      name: "HomePage", 
+      component: HomePage
+    },
+
     {
       path: "/",
       alias: "/login",
