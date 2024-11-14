30d9897eef5db4d9e752423992080e5aa6393252
Logan Murrie <logan.murrie@eagles.oc.edu>
2024-11-14 01:17:00 -0600

    Created the Awards Component For the Resume

---
+++ src/components/AwardsInfo.vue
@@ -0,0 +1,101 @@
+<script setup>
+const required = (label) => (value) => !!value || `The ${label} field is required.`;
+</script>
+<template>
+    <div>
+        <v-form ref="AwardForm">
+            <v-row align="center" justify="center">
+                <v-card class="mx-auto" width="440" height="600"
+                    :style="'overflow-y: scroll'">
+                    <v-card-title style="text-align:center">
+                        <span class="text-h6">Awards</span>
+                    </v-card-title>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-card-text>
+                        <v-row v-for="(item, index)  in Award" :key="index">
+                            <v-col cols="100">
+                                <v-text-field class="formField" v-model="item.AwardName" label="Award Name*"
+                                    :rules="[required('Award Name')]"></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-textarea class="formField" v-model="item.AwardDesc" label="Award Description*" 
+                                :rules="[required('Award Description')]"></v-textarea>
+                            </v-col>
+
+                            <div v-if="index > 0" class="mx-auto">
+                                <v-btn @click="removeAward(index)" style="float:left">
+                                    <v-icon icon="mdi-minus" style="font-size: 20px;"></v-icon>
+                                </v-btn>
+                            </div>
+                            <v-divider class="my-5"></v-divider>
+                            <v-spacer></v-spacer>
+                        </v-row>
+                        <v-row>
+                            <div class="mx-auto">
+                                <v-btn @click="addNewAward" style="float:left" class="mx-5">
+                                    <v-icon icon="mdi-plus" style="font-size: 20px;"></v-icon>
+                                </v-btn>
+                            </div>
+                        </v-row>
+                    </v-card-text>
+                    <v-spacer></v-spacer>
+                    <v-card-actions style="position: relative; bottom: 0;">
+                        <div>
+                            <v-btn @click="getPrevious" style="float:left">
+                                <v-icon icon="mdi-chevron-left" style="font-size: 30px;"></v-icon>
+                            </v-btn>
+                        </div>
+                        <v-spacer></v-spacer>
+                        <div>
+                            <v-btn variant="outlined" @click="skip" class="mx-auto">
+                                Skip
+                            </v-btn>
+                        </div>
+                        <v-spacer></v-spacer>
+                        <div>
+                            <v-btn @click="submitForm" style="float:right">
+                                <v-icon icon="mdi-chevron-right" style="font-size: 30px;"></v-icon>
+                            </v-btn>
+                        </div>
+                    </v-card-actions>
+                </v-card>
+            </v-row>
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        Award: {
+            type: Object,
+        },
+    },
+    methods: {
+        async submitForm() {
+            const { valid } = await this.$refs.AwardForm.validate();
+            if (valid) {
+                //insert backend logic here
+                this.$emit('getNext');
+            }
+        },
+        addNewAward() {
+            this.Award.push({
+                id: 0,
+                AwardName: "",
+                AwardDesc: ""
+            })
+        },
+        removeAward(index) {
+            this.Award.splice(index, 1);
+        },
+        getPrevious() {
+            this.$emit('getPrevious');
+        },
+        skip() {
+            this.Award.splice(0, this.Award.length);
+            this.$emit('getNext');
+        },
+    }
+}
+</script>
--- src/views/CreateResume.vue
+++ src/views/CreateResume.vue
@@ -13,6 +13,9 @@
     <div v-if="componentSelected == 3">
       <InterestInfo @getNext="getNext" :Interest="Interest"  @getPrevious="getPrevious"/>
     </div>
+    <div v-if="componentSelected == 4">
+      <AwardsInfo @getNext="getNext" :Award="Award"  @getPrevious="getPrevious"/>
+    </div>
   </v-container>
 </template>
 
@@ -21,6 +24,7 @@ import PersonalInfo from "../components/PersonalInfo.vue"
 import EducationInfo from "../components/EducationInfo.vue"
 import ExperienceInfo from "../components/ExperienceInfo.vue"
 import InterestInfo from "../components/InterestInfo.vue"
+import AwardsInfo from "../components/AwardsInfo.vue"
 import { reactive, ref } from 'vue';
 
 export default {
@@ -28,7 +32,8 @@ export default {
     PersonalInfo,
     EducationInfo,
     ExperienceInfo,
-    InterestInfo
+    InterestInfo,
+    AwardsInfo
   },
   setup() {
     const Person = reactive({
@@ -66,8 +71,15 @@ export default {
         InterestDesc: "",
       }
     ]);
+    const Award = reactive([
+      {
+        id: 0,
+        AwardName: "",
+        AwardDesc: "",
+      }
+    ]);
     const componentSelected = ref(0);
-    return { Person, ContactInfo, Education, Interest, componentSelected, Experience };
+    return { Person, ContactInfo, Education, Interest, componentSelected, Experience, Award };
   },
   methods: {
     getNext() {
